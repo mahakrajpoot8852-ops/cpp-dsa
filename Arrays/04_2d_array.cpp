26262c4ce3930e9d1ab599e1189c5e7b4c183860
Mahak Rajpoot <mahakrajpoot8852@gmail.com>
2026-06-26 23:58:49 +0530

    Add 2D Array Program

---
+++ Arrays/04_2d_array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[2][2];
+
+    cout << "Enter 4 elements:" << endl;
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "Matrix is:" << endl;
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
