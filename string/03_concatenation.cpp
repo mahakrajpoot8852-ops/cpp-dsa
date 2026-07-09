8fd0416df48730a0d14958b40611f13180f85b60
Mahak Rajpoot <mahakrajpoot8852@gmail.com>
2026-07-09 21:29:47 +0530

    Create 03_concatenation.cpp

---
+++ string/03_concatenation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string first = "Hello";
+    string second = "World";
+
+    string result = first + " " + second;
+
+    cout << result << endl;
+
+    return 0;
+}
